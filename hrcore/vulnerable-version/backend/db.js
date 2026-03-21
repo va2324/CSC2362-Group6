f8cc3376e937e85dc920fb9460882741b5bd0d47
va2324 <VasuK1234@outlook.com>
2026-03-20 21:34:47 -0500

    Set up proper database connection

--- hrcore/vulnerable-version/backend/db.js
+++ hrcore/vulnerable-version/backend/db.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_NAME || 'hrcore',
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
+  password: process.env.DB_PASSWORD || 'Elephant#18',
 });
 
 module.exports = { pool };
